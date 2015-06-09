38c1f69c8f6f492a1a9f9568468260cd4ca013ae
Nahuel Jose <nahuejose@openmailbox.org>
2015-06-09 14:33:09 +0200

    home tab

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,18 +2,32 @@ angular.module('access.controllers', [])
 
 .controller('SignUpCtrl', function($scope, $state) {
   
-  // $scope.signIn = function(user) {
-  //   console.log('Sign-In', user);
-  //   $state.go('tabs.home');
-  // };
+  $scope.user = {};
+  $scope.verification = {};
+
+  $scope.signUp = function(user,verification) {
+  	if(user.userPassword==verification.password){
+	  	console.log(user.userNickname);
+	    console.log(user.userName);
+	    console.log(user.userSurname);
+	    console.log(user.userEmail);
+	    console.log(user.userPassword);
+	  	//add to de DB and send to logIn page 
+	    $state.go('tab.logIn');
+  	}
+  };
   
 })
 
 .controller('LogInCtrl', function($scope, $state) {
   
-  // $scope.logIn = function(user) {
-  $scope.logIn = function() {
-    // console.log('Sign-In', user);
+  $scope.user = {};
+
+  $scope.logIn = function(user) {
+
+		console.log(user.userNickname);
+		console.log(user.userPassword);
+		//verification on DB and send to Home page
     $state.go('tabsHome.home');
   };
 })
