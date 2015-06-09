6d856304eaf5eac28f29939c9c3b84201ce28401
Nahuel Jose <nahuejose@openmailbox.org>
2015-06-09 15:45:40 +0200

    subtabs de home and account

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,39 +1,50 @@
 angular.module('access.controllers', [])
 
-.controller('SignUpCtrl', function($scope, $state) {
-  
-  $scope.user = {};
-  $scope.verification = {};
-
-  $scope.signUp = function(user,verification) {
-  	if(user.userPassword==verification.password){
-	  	console.log(user.userNickname);
-	    console.log(user.userName);
-	    console.log(user.userSurname);
-	    console.log(user.userEmail);
-	    console.log(user.userPassword);
-	  	//add to de DB and send to logIn page 
-	    $state.go('tab.logIn');
-  	}
-  };
-  
-})
-
 .controller('LogInCtrl', function($scope, $state) {
   
-  $scope.user = {};
+	$scope.user = {};
 
-  $scope.logIn = function(user) {
+	$scope.logIn = function(user) {
 
 		console.log(user.userNickname);
 		console.log(user.userPassword);
 		//verification on DB and send to Home page
-    $state.go('tabsHome.home');
-  };
+		$state.go('tabsHome.home');
+	};
 })
+
+.controller('SignUpCtrl', function($scope, $state) {
+  
+	$scope.user = {};
+	$scope.verification = {};
+
+	$scope.signUp = function(user,verification) {
+		if(user.userPassword==verification.password){
+			console.log(user.userNickname);
+			console.log(user.userName);
+			console.log(user.userSurname);
+			console.log(user.userEmail);
+			console.log(user.userPassword);
+			//add to de DB and send to logIn page 
+			$state.go('tab.logIn');
+		}
+	};
+})
+
+.controller('ForgotPasswordCtrl', function($scope) {})
 
 .controller('HomeCtrl', function($scope) {})
 
-.controller('AccountCtrl', function($scope) {})
+.controller('AccountCtrl', function($scope, $state) {
+
+ //  	$scope.lookForHistory = function() {
+ //  		console.log("going");
+ //    	$state.go('historyDetail');
+	// };
+})
 
 .controller('SettingsCtrl', function($scope) {})
+
+.controller('HistoryDetailCtrl', function($scope) {})
+
+.controller('EventsDetailCtrl', function($scope) {})
