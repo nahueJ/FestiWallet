a8210389f0ed3311be73a1bbe29ffd6b077fcce3
tinchoAbr <tincho_abr@hotmail.com.ar>
2015-06-15 23:31:10 +0200

    qr+email

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -31,8 +31,6 @@ angular.module('access.controllers', ['ionic','ngCordova'])
 	};
 })
 
-.controller('ForgotPasswordCtrl', function($scope) {})
-
 .controller('HomeCtrl', function($scope, $state) {
   
 	$scope.logOut = function() {
@@ -53,23 +51,67 @@ angular.module('access.controllers', ['ionic','ngCordova'])
 .controller('BuyCtrl', function($scope) {})
 
 .controller('GainCtrl', function($scope, $cordovaBarcodeScanner, $cordovaSms) {
-	$scope.leerCodigo = function () {
-		$cordovaBarcodeScanner.scan().then (function(imagenEscaneada){
-			alert(imagenEscaneada.text);
-		}, function(error){
-			alert("Ha ocurrido un error: "+error);
-		});
-	}
 
-	$scope.enviarSMS = function(){
-    	$cordovaSms.send('phonenumber', 'SMS content', options).then(function() {
-        	alert(phonenumber);
-      	}, function(error) {
-        	alert(phonenumber);
-      	});
-    }
+	angular.module('lector.controllers',['ionic', 'ngCordova'])
+		$scope.leerCodigo = function(){
+				$cordovaBarcodeScanner.scan().then (function(imagenEscaneada){
+					alert(imagenEscaneada.text);
+				}, function(error){
+					alert("Ha ocurrido un error: "+error);
+				});
+			}
+
+		$scope.enviarSMS = function(){
+	    	$cordovaSms.send('phonenumber', 'SMS_content', options).then(function() {
+	        	alert('Enviado a: '+phonenumber+' con el mensaje '+SMS_content);
+	      	}, function(error) {
+	        	alert(phonenumber);
+	      	});
+	    }
 })
 
+.controller('ForgotPasswordCtrl', function($scope, $cordovaEmailComposer) {
+	
+	angular.module('mail.controlles',['ionic', 'ngCordova'])
+	 	$scope.enviarMail = function(){
+			if(window.plugins && window.plugins.emailComposer){
+			window.plugins.emailComposer.showEmailComposerWithCallback(function(result){
+			  console.log("Email succes");
+			},
+			  "subject here",
+			  "",
+			  ["tincho_abr@hotmail.com.ar"],
+			  null,
+			  null,
+			  false,
+			  null,
+			  null
+			);
+			}
+
+	 		alert("Your mail was send");
+			$cordovaEmailComposer.isAvailable().then(function() {
+		 		alert("enviar un mail loco");
+			}, function (error) {
+				alert("Ha ocurrido un error: "+error);
+			});
+	 	}
+	 	
+	 	var email = {
+		    to: 'tincho_abr@hotmail.com.ar',
+		    cc: 'test',
+		    bcc: ['john@doe.com', 'jane@doe.com'],
+		    attachments: [
+		      'file://img/logo.png',
+		      'res://icon.png',
+		      'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+		      'file://README.pdf'
+		    ],
+		    subject: 'Cordova Icons',
+		    body: 'How are you? Nice greetings from Leipzig',
+		    isHtml: true
+		};
+})
 
 .controller('HistoryDetailCtrl', function($scope) {})
 
